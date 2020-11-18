c6bfa4a36dd127ba4f13cab8884b8c17809ab310
Ayberk Selçuk <ayberkselcuk@Basak-MacBook-Pro.local>
2020-11-18 20:00:33 +0300

    Rubocop düzeltildi.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,25 +2,21 @@
 
 # QuestionsController
 class QuestionsController < ApplicationController
-  
-  before_action :find_question, only: [:show, :edit, :update, :destroy]
-
+  before_action :find_question, only: %i[show edit update destroy]
 
   def index
-  	@questions = Question.all.order("created_at DESC")
+    @questions = Question.all.order('created_at DESC')
   end
 
-  def edit
-  end
-  
+  def edit; end
+
   def update
     if @question.update(question_params)
-    redirect_to @question
-   else
-    render 'edit'
-   end
+      redirect_to @question
+    else
+      render 'edit'
+    end
   end
-  
 
   def destroy
     @question.destroy
@@ -28,32 +24,31 @@ class QuestionsController < ApplicationController
   end
 
   def new
-   if user_signed_in?
-     @question = current_user.questions.build
-   else
-     redirect_to new_user_session_path
-   end
+    if user_signed_in?
+      @question = current_user.questions.build
+    else
+      redirect_to new_user_session_path
+    end
   end
 
- def create
-   @question = current_user.questions.build(question_params)
- if @question.save
-   redirect_to @question
-  else
-   render 'new'
-  end
- end
-  
-  def show
+  def create
+    @question = current_user.questions.build(question_params)
+    if @question.save
+      redirect_to @question
+    else
+      render 'new'
+    end
   end
 
- private
+  def show; end
+
+  private
 
   def question_params
     params.require(:question).permit(:title, :content)
   end
 
   def find_question
- 	  @question = Question.find(params[:id])
+    @question = Question.find(params[:id])
   end
 end
